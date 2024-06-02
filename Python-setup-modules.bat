a18b0ec5784de1dac8038d9571aabdc630228419
HoangECG <63526968+HoangECG@users.noreply.github.com>
2024-06-02 16:34:50 +0700

    a

---
+++ Python-setup-modules.bat
@@ -0,0 +1,8 @@
+python -m pip install requests
+python -m pip install numpy
+python -m pip install pillow
+python -m pip install opencv-python
+python -m pip install obsws_python
+python -m pip install fastapi
+python -m pip install pyautogui
+python -m pip install pynput
